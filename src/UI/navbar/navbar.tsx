e7fed6b7851db866f1138eb21a5d2c2a2eeaf508
Muhless <nuryadi.muhta@gmail.com>
2024-03-04 15:20:53 +0700

    first commit

---
+++ src/UI/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Buy</li>
+                <li>Sell</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
