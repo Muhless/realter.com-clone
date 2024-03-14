375e9af2d5ac03a57bc8b620885f56799d931e7c
Muhless <nuryadi.muhta@gmail.com>
2024-03-14 16:12:03 +0700

    update content

--- src/components/contents/Contents.tsx
+++ src/components/contents/Contents.tsx
@@ -2,11 +2,18 @@ import HouseContent from "./house/House"
 
 const Content = () => {
     return (
-        <div className="w-screen box-border flex flex-col">
-            <h1 className="font-bold text-xl justify-center items-center text-center">
+        <div className="flex flex-col mx-32">
+            <h1 className="font-black text-xl mt-16 mb-6">
                 Browse homes in Los Angeles, CA
             </h1>
-            <div>
+            <div className="grid grid-cols-4 items-center">
+                <HouseContent />
+                <HouseContent />
+                <HouseContent />
+                <HouseContent />
+                <HouseContent />
+                <HouseContent />
+                <HouseContent />
                 <HouseContent />
             </div>
         </div>
--- src/components/contents/house/House.tsx
+++ src/components/contents/house/House.tsx
@@ -1,14 +1,28 @@
+'use client'
+
 import Image from "next/image"
 
 const HouseContent = () => {
+    const houseImage = ({ src }) => {
+        return `/images/houses/${src}`
+    }
     return (
         <div>
+            <div className="flex flex-row  mx-10 justify-between items-center">
+                <p className="font-bold flex items-center text-center">
+                    New Listings
+                </p>
+                <p>1920</p>
+            </div>
             <Image
-                src={'/images/houses/pexels-binyamin-mellish-1396122.jpg'}
+                loader={houseImage}
+                src='pexels-binyamin-mellish-1396122.jpg'
                 alt=""
-                width={100}
+                width={1920}
                 height={100}
-                />
+                className="h-52 w-[300px] rounded-xl mb-4 shadow-inner"
+            />
+
         </div>
     )
 }
